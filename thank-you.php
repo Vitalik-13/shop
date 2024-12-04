439b753e523d30201a8a5573197d026f579df1ea
Vitalik <vitalikgermak6@gmail.com>
2024-12-04 14:52:53 +0200

    add new style

--- thank-you.php
+++ thank-you.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 // Отримуємо дані з форми
 $id = $_POST['id'] ?? 'ID не вказано';
 $name = $_POST['name'] ?? 'Ім\'я не вказано';
@@ -13,14 +16,24 @@ $price = $_POST['price'] ?? 'Ціна не вказана';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
     <title>Дякуємо за замовлення</title>
 </head>
 <body>
-    <h1>Дякуємо за ваше замовлення!</h1>
+    <div class="container php">
+    <h1 class="php-title">Дякуємо за ваше замовлення!</h1>
+    <p class="php-paragraph">Менеджер зв'яжеться з вами найближчим часом</p>
+    <a href="./index.html" class="link-php">Замовити ще</a>
+
+<!-- це можна видалити ================================================== -->
     <p><strong>Ім'я:</strong> <?php echo htmlspecialchars($name); ?></p>
     <p><strong>Телефон:</strong> <?php echo htmlspecialchars($phone); ?></p>
     <p><strong>Стаття:</strong> <?php echo htmlspecialchars($article); ?></p>
     <p><strong>Ціна:</strong> <?php echo htmlspecialchars($price); ?></p>
     <p><strong>id товару</strong> <?php echo htmlspecialchars($id); ?></p>
+<!-- це можна видалити ================================================== -->
+
+    </div>
 </body>
 </html>
